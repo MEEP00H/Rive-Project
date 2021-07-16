d57d87a2b60a681c582c18b7695879851fe05dbe
Pooh <sophon@punchup.world>
2021-07-16 16:57:19 +0700

    set up rive

---
+++ src/App.svelte
@@ -0,0 +1,27 @@
+
+<script >
+  	import {Rive} from 'rive-js'
+	import {onMount} from 'svelte'
+	
+	let canvas
+	onMount(() => {
+		rive = new Rive({
+		   src: 'rive_test.riv',
+		  canvas:canvas,
+		  autoplay: true,
+	  });
+	  
+	  
+	})
+</script>
+<main>
+<canvas bind:this={canvas} width={1280} height={722}></canvas>
+</main>
+
+<style>
+	main{
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+</style>
